7b580e094dc5b3a798fcc5c862700fa85a1a04e1
Jerry Agbesi <jerryeagbesi@gmail.com>
2024-09-22 22:53:09 +0000

    add prometheus monitoring

--- Services/showtimes/cmd/app/main.go
+++ Services/showtimes/cmd/app/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+
+	"github.com/jerryAgbesi/microservices-demo/showtimes/pkg/models/mongodb"
+)
+
+type application struct {
+	errorLog  *log.Logger
+	infoLog   *log.Logger
+	showtimes *mongodb.ShowTimeCollection
+}
+
+func main() {
+	// infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	// errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+}
--- Services/showtimes/pkg/models/mongodb/showtimes.go
+++ Services/showtimes/pkg/models/mongodb/showtimes.go
@@ -1,1 +1,32 @@
 package mongodb
+
+import (
+	"context"
+
+	"github.com/jerryAgbesi/microservices-demo/showtimes/pkg/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ShowTimeCollection struct {
+	C *mongo.Collection 
+}
+
+func (c *ShowTimeCollection) All() ([]models.ShowTimes,error){
+	ctx := context.TODO()
+	st := []models.ShowTimes{}
+
+	showtimeCursor, err := c.C.Find(ctx,bson.M{})
+
+	if err != nil{
+		return nil,err
+	}
+
+	err = showtimeCursor.All(ctx,&st)
+	if err != nil{
+		return nil,err
+	}
+
+	return st,err
+
+}
